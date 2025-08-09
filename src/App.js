7b64bd15f6c5f25b9f5716aefbb7f5df9e4f2048
muratcankargi <muratcankargi@hotmail.com>
2025-08-09 21:02:31 +0300

    Add 2 pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import CreateRoom from './pages/CreateRoom';
 import HomePage from './pages/HomePage';
+import GameRoom from './pages/GameRoom';
+import JoinRoom from './pages/JoinRoom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create-room" element={<CreateRoom />} />
+        <Route path="/rooms/:roomId" element={<GameRoom />} />
+        <Route path="/join-room" element={<JoinRoom />} />
 
       </Routes>
     </Router>
