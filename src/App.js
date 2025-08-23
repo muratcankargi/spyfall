1eb2ab1b3024972d2cb77971150852785298db88
muratcankargi <muratcankargi@hotmail.com>
2025-08-23 20:46:37 +0300

    Update App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CreateRoom from './pages/CreateRoom';
 import HomePage from './pages/HomePage';
 import GameRoom from './pages/GameRoom';
 import JoinRoom from './pages/JoinRoom';
+import GamePlay from './pages/GamePlay';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/create-room" element={<CreateRoom />} />
         <Route path="/rooms/:roomId" element={<GameRoom />} />
         <Route path="/join-room" element={<JoinRoom />} />
+        <Route path="/play/:roomId" element={<GamePlay />} />
 
       </Routes>
     </Router>
