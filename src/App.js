44baf24d0996a38689152ce5368e38db7a52a2ae
muratcankargi <muratcankargi@hotmail.com>
2025-08-24 20:04:07 +0300

    update App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import HomePage from './pages/HomePage';
 import GameRoom from './pages/GameRoom';
 import JoinRoom from './pages/JoinRoom';
 import GamePlay from './pages/GamePlay';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
@@ -14,9 +16,10 @@ function App() {
         <Route path="/rooms/:roomId" element={<GameRoom />} />
         <Route path="/join-room" element={<JoinRoom />} />
         <Route path="/play/:roomId" element={<GamePlay />} />
-
       </Routes>
+      <ToastContainer position="top-right" autoClose={3000} />
     </Router>
+    
   );
 }
 
