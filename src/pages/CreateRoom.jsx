62ee236afa194faf10ade98ddcf1d2cc87679696
Murat Can Kargi <muratcankargi@hotmail.com>
2025-09-03 15:32:05 +0000

    Update for CI/CD and production

--- src/pages/CreateRoom.jsx
+++ src/pages/CreateRoom.jsx
@@ -8,7 +8,6 @@ function CreateRoom() {
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
     const API_URL = process.env.REACT_APP_API_URL;
-
     const handleSubmit = async (e) => {
         e.preventDefault();
         setError('');
